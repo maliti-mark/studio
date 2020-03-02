8d27991990e5b15302d65f1abe7f4ca633fad0f3
markmaliti <61108820+maliti-mark@users.noreply.github.com>
2020-03-02 18:06:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $("img").show();
- });
+  $("#image1").click(function() {
+      $("#design").fadeToggle(1000);
+  });
+  $("#image2").click(function() {
+      $("#develop").slideToggle(1000);
+  });
+  $("#image3").click(function() {
+      $("#Products").toggle(1000);
+  });
+  $('#form form').submit(function(){
+    var name = $('#name').val();
+     alert("Your message "+name+ " has been sent");
+  });
+  $("#portfolio .col-md-2").hover(function(){
+    $(this).find(".display").css("display","flex");
+},function(){
+    $(this).find(".display").hide();
 });
+});
+
